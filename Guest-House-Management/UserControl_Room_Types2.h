2b1388dae1a5e82e1a6fbbe5cf969be7d59cc16b
cgupta3131 <cgupta3131@gmail.com>
2019-03-28 13:40:30 +0530

    Room Types2 Added and Changed location of Textboxes

--- Guest-House-Management/UserControl_Room_Types2.h
+++ Guest-House-Management/UserControl_Room_Types2.h
@@ -72,10 +72,15 @@ namespace GuestHouseManagement {
 			// 
 			// Txt_Room_Type
 			// 
+			this->Txt_Room_Type->BackColor = System::Drawing::SystemColors::Window;
+			this->Txt_Room_Type->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 8.25F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point, 
+				static_cast<System::Byte>(0)));
+			this->Txt_Room_Type->ForeColor = System::Drawing::Color::Black;
 			this->Txt_Room_Type->Location = System::Drawing::Point(109, 106);
 			this->Txt_Room_Type->Name = L"Txt_Room_Type";
 			this->Txt_Room_Type->Size = System::Drawing::Size(113, 20);
 			this->Txt_Room_Type->TabIndex = 0;
+			this->Txt_Room_Type->TextChanged += gcnew System::EventHandler(this, &UserControl_Room_Types2::Txt_Room_Type_TextChanged);
 			// 
 			// Btn_Add
 			// 
@@ -115,54 +120,89 @@ namespace GuestHouseManagement {
 
 		void button_click(System::Object^  sender, System::EventArgs^  e)
 		{
+			int f = 0;
 			for(int i=0;i<vec.size();i++)
 			{
-
 				TextBox ^tb = (TextBox^)this->Controls["TextBox" + (i)];
 				String ^str = tb->Text;
-				
-				//MessageBox::Show(str);
+				if(tb->Text == "")
+				{
+					f=1;
+					MessageBox::Show("Price field can't be empty");
+					break;
+				}
+					
+			}
+
+			if(f == 0)
+			{
+				for(int i=0;i<vec.size();i++)
+				{
+
+					TextBox ^tb = (TextBox^)this->Controls["TextBox" + (i)];
+					String ^str = tb->Text;
+
+					OleDb::OleDbConnection ^ DB_Connection = gcnew OleDb::OleDbConnection();
+					DB_Connection->ConnectionString = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=GuestHouse.accdb";
+					DB_Connection->Open();
+					String ^ insertString = "insert into User_Room_Price([User_Types],[Room_Types],[Price]) VALUES('" +vec[i]+ "', '" +room+ "', '" +str+ "' );";
+
+					OleDbCommand ^ cmd = gcnew OleDbCommand(insertString, DB_Connection);
+
+					cmd->Parameters->Add(gcnew OleDbParameter("@User_Types",Convert::ToString(vec[i])));
+					cmd->Parameters->Add(gcnew OleDbParameter("@Room_Types",Convert::ToString(room)));
+					cmd->Parameters->Add(gcnew OleDbParameter("@Price",Convert::ToString(str)));
+
+					cmd->ExecuteNonQuery();
+					DB_Connection->Close();
+
+				}
+
+				String ^str2 = Txt_Occupancy->Text;
 				OleDb::OleDbConnection ^ DB_Connection = gcnew OleDb::OleDbConnection();
 				DB_Connection->ConnectionString = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=GuestHouse.accdb";
 				DB_Connection->Open();
-				String ^ insertString = "insert into User_Room_Price([User_Types],[Room_Types],[Price]) VALUES('" +vec[i]+ "', '" +room+ "', '" +str+ "' );";
-
+				String ^ insertString = "insert into Room_Types([Room_Type], [Occupancy]) VALUES('" +room+ "', '" +str2+ "' );";
 				OleDbCommand ^ cmd = gcnew OleDbCommand(insertString, DB_Connection);
-
-				cmd->Parameters->Add(gcnew OleDbParameter("@User_Types",Convert::ToString(vec[i])));
-				cmd->Parameters->Add(gcnew OleDbParameter("@Room_Types",Convert::ToString(room)));
-				cmd->Parameters->Add(gcnew OleDbParameter("@Price",Convert::ToString(str)));
-
+				cmd->Parameters->Add(gcnew OleDbParameter("@Room_Type",Convert::ToString(room)));
 				cmd->ExecuteNonQuery();
 				DB_Connection->Close();
 
+				MessageBox::Show("Room has been successfully Added");
+				this->Controls->Clear();
+				this->InitializeComponent();
 			}
+		}
 
-			String ^str2 = Txt_Occupancy->Text;
-			OleDb::OleDbConnection ^ DB_Connection = gcnew OleDb::OleDbConnection();
-			DB_Connection->ConnectionString = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=GuestHouse.accdb";
-			DB_Connection->Open();
-			String ^ insertString = "insert into Room_Types([Room_Type], [Occupancy]) VALUES('" +room+ "', '" +str2+ "' );";
-			OleDbCommand ^ cmd = gcnew OleDbCommand(insertString, DB_Connection);
-			cmd->Parameters->Add(gcnew OleDbParameter("@Room_Type",Convert::ToString(room)));
-			cmd->ExecuteNonQuery();
-			DB_Connection->Close();
-
+		void back_button_click(System::Object^  sender, System::EventArgs^  e)
+		{
 			this->Controls->Clear();
 			this->InitializeComponent();
 		}
 
 
+
 	public: System::Void UserControl_Room_Types_Load(System::Object^  sender, System::EventArgs^  e) {
 
-
+					
 			}
 
 	private: System::Void Btn_Add_Click(System::Object^  sender, System::EventArgs^  e) {
 
 				 try{
+					 
+					 if(Txt_Room_Type->Text == "")
+					 {
+						 MessageBox::Show("Room-Type can't be empty");
+						 return;
+					 }
 
-					 Btn_Add->Visible = false;
+					 if(Txt_Occupancy->Text == "")
+					 {
+						 MessageBox::Show("Occupancy can't be empty");
+						 return;
+					 }
+
 					 OleDb::OleDbConnection ^ DB_Connection = gcnew OleDb::OleDbConnection();
 					 DB_Connection->ConnectionString = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=GuestHouse.accdb";
 					 DB_Connection->Open();
@@ -177,12 +217,13 @@ namespace GuestHouseManagement {
 					 {
 						 MessageBox::Show("Room Type Already Exists");
 						 DB_Connection->Close();
-
 					 }
 					 else
 					 {
 						 DB_Connection->Close();
-						 MessageBox::Show(room);
+						 Btn_Add->Visible = false;
+						 Txt_Room_Type->Visible = false;
+						 Txt_Occupancy->Visible = false;
 						 if(String::IsNullOrWhiteSpace(room))
 							 MessageBox::Show("Please Enter Some Room Type");
 						 else
@@ -207,15 +248,19 @@ namespace GuestHouseManagement {
 
 								 Label ^ lb = gcnew Label();
 								 lb->Text = vec[i];
-								 lb->Location = System::Drawing::Point(20,70*(i+1));
+								 lb->Location = System::Drawing::Point(20,50*(i+1));
+								 lb->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 14.25F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point, 
+									 static_cast<System::Byte>(0)));
 
 								 this->Controls->Add(lb);
 
 								 TextBox ^ tb = gcnew TextBox();
 								 tb->Name = "TextBox" + i;
-								 tb->Width = 270;
+								 tb->Width = 100;
 								 tb->Height = 50;
-								 tb->Location = System::Drawing::Point(120,70*(i+1));
+								 tb->Location = System::Drawing::Point(120,50*(i+1));
+								 tb->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 14.25F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point, 
+									 static_cast<System::Byte>(0)));
 								 this->Controls->Add(tb);
 							 }
 
@@ -228,12 +273,19 @@ namespace GuestHouseManagement {
 							 btn->Click += gcnew System::EventHandler(this,&UserControl_Room_Types2::button_click);
 							 this->Controls->Add(btn);
 
+							 Button ^ btn2 = gcnew Button();
+							 btn2->Text = "Go Back";
+							 btn2->Name = "Btn_Back";
+
+							 btn2->ForeColor = System::Drawing::SystemColors::WindowText;
+							 btn2->Location = System::Drawing::Point(473, 204);
+							 btn2->Click += gcnew System::EventHandler(this,&UserControl_Room_Types2::back_button_click);
+							 this->Controls->Add(btn2);
+
 							 DB_Connection->Close();
 
 						 }
 					 }
-
-
 
 				 }
 
@@ -242,7 +294,17 @@ namespace GuestHouseManagement {
 					 MessageBox::Show(ex->Message);
 				 }
 			 }
-	};
+	private: System::Void Txt_Room_Type_TextChanged(System::Object^  sender, System::EventArgs^  e) {
+
+
+			 }
+	private: System::Void Txt_Room_Type_Enter(System::Object^  sender, System::EventArgs^  e) {
+
+				 if(Txt_Room_Type->Text == "Enter some text here")
+					 Txt_Room_Type->Text = "";
+			 }
+	
+};
 
 
 }
